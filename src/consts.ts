ba067b45c0a47c8baf3ed9f88086b697d7487e7f
William Sullivan <william@symbotics.co.uk>
2025-06-06 20:55:28 +0100

    Just a bit of playing.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Loopy Owl Comix";
+export const SITE_DESCRIPTION = "Welcome to my website! I do art and comics!";
